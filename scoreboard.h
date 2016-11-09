84aea10f779f3ce88d10f79aa2ef8fa51eeb0ddb
kdavis99 <kdavis99@mail.csuchico.edu>
2016-11-09 11:23:27 -0800

    added constructor for scoreboard.

--- scoreboard.h
+++ scoreboard.h
@@ -8,6 +8,10 @@ using namespace std;
 
 class Scoreboard : public IScoreboard {
    public:
+      Scoreboard(int competitor, int period, int score);
    private:
+      int m_competitor;
+      int m_period;
+      int m_score;
 };
 #endif
