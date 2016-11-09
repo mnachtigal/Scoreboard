d85037c7ee2594af27fed69b9cbb0667849fff72
Matt Nachtigal <mnachtigal@mail.csuchico.edu>
2016-11-09 11:28:41 -0800

    added functions to .cpp file

--- scoreboard.cpp
+++ scoreboard.cpp
@@ -3,3 +3,20 @@
 
 using namespace std;
 
+
+void setScore(int competitor, int period, int score)
+{
+
+}
+int Scoreboard::getScore(int competitor, int period)
+{
+
+}
+int Scoreboard::getTotalScore(int competitor)
+{
+
+}
+void Scoreboard::clearScoreboard()
+{
+
+}
