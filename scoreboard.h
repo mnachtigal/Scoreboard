47ecf5f34d125b3c834fc8afd923328d8f1b26ee
kdavis99 <kdavis99@mail.csuchico.edu>
2016-11-09 11:14:35 -0800

    scoreboard.h created, added inheritance from IScoreboard

---
+++ scoreboard.h
@@ -0,0 +1,13 @@
+#ifndef SCOREBOARD_H
+#define SCOREBOARD_H
+
+#include <iostream>
+#include "IScoreboard.h"
+using namespace std;
+
+
+class Scoreboard : public IScoreboard {
+   public:
+   private:
+};
+#endif
